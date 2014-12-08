e51a345a856dfb02f9cc5fc38d6517e7e5d7273c
David Douard <david.douard@logilab.fr>
2014-12-08 21:08:33 +0100

    probably to kill

--- reporting_ui/reportviewer.py
+++ reporting_ui/reportviewer.py
@@ -3,8 +3,8 @@
 
 """
 
-from externals.formlayout.formlayout import QDialog, QWebView, QUrl, QVBoxLayout, QDialogButtonBox
-
+from externals.formlayout.formlayout import (QDialog, QWebView, QUrl, QVBoxLayout,
+                                             QDialogButtonBox, QTextEdit)
 
 class ReportViewer(QDialog):
     def __init__(self, url, parent=None):
@@ -20,3 +20,21 @@ class ReportViewer(QDialog):
         bb.rejected.connect(self.reject)
         bb.accepted.connect(self.accept)
         
+class ComputationProgressViewer(QDialog):
+    def __init__(self, parent=None):
+        super(ComputationProgressViewer, self).__init__(parent)
+        l = QVBoxLayout(self)
+        self.tv = QTextEdit(readOnly=True)
+        self.tv.setFontFamily('courrier')
+        self.resize(800,600)
+        l.addWidget(self.tv)
+
+        bb = QDialogButtonBox(QDialogButtonBox.Close)
+        l.addWidget(bb)
+
+        bb.rejected.connect(self.reject)
+        bb.accepted.connect(self.accept)
+    
+    def write(self, value):
+        self.tv.append(value)
+    
