3d88c76d3af0753505d811cb3f662cbf5626dd72
David Douard <david.douard@logilab.fr>
2016-01-22 14:55:24 +0100

    [reporting] insert glass_is brain pictures in the HTML report

--- reporting_ui/generate_report.py
+++ reporting_ui/generate_report.py
@@ -11,7 +11,7 @@ import json
 import nibabel
 
 from nilearn import datasets
-from nilearn.plotting import plot_stat_map
+from nilearn.plotting import plot_stat_map, plot_glass_brain
 from nilearn.decomposition.canica import CanICA
 
 from externals.formlayout.formlayout import fedit, QApplication, QWebView
@@ -59,43 +59,52 @@ def get_fitted_canica(func_files, **params):
     return canica
 
 
-def generate_images(components_img, n_components, images_dir):
+def generate_images(components_img, n_components, images_dir, glass=False):
     # Remove existing images
     if os.path.exists(images_dir):
         shutil.rmtree(images_dir)
     os.makedirs(images_dir)
     output_filenames = [osp.join(images_dir, 'IC_{}.png'.format(i))
                         for i in range(n_components)]
+        
 
     for i, output_file in enumerate(output_filenames):
         plot_stat_map(nibabel.Nifti1Image(components_img.get_data()[..., i],
                                           components_img.get_affine()),
                       display_mode="z", title="IC %d" % i, cut_coords=7,
                       colorbar=False, output_file=output_file)
+    if glass:
+        output_filenames = [osp.join(images_dir, 'glass_IC_{}.png'.format(i))
+                            for i in range(n_components)]
+        for i, output_file in enumerate(output_filenames):
+            plot_glass_brain(nibabel.Nifti1Image(components_img.get_data()[..., i],
+                                                 components_img.get_affine()),
+                             display_mode="ortho", title="IC %d" % i,
+                             output_file=output_file)
 
-    return output_filenames
 
 
 def generate_report(params_dict, img_src_filenames):
     report = api.Report()
 
-    report.add(api.Section('Model parameters')).add(
-        api.Table(params_dict.iteritems(), headers=('Parameter', 'Value'))
-    )
+    report.add(api.Section('Model parameters'),
+               api.Table(params_dict.iteritems(),
+                         headers=('Parameter', 'Value')))
+
 
     for counter, filename in enumerate(img_src_filenames):
-        caption = 'Some caption for component {} goes here'.format(counter)
+        caption = 'component {}'.format(counter)
         section = api.Section('Component #{}'.format(counter))
-        section.add(
-            api.Paragraph('This is some paragraph text related to '
-                          'component #{}'.format(counter))
-            )
+        #section.add(
+        #    api.Paragraph('This is some paragraph text related to '
+        #                  'component #{}'.format(counter))
+        #    )
         section.add(
             api.Image(filename, caption=caption)
             )
         report.add(section)
     report.add(api.Section('About').add(api.Paragraph('This report has been generated using the nilearn toolkit.')))
-    report.add(api.Section('Contact').add(api.Paragraph('See https://github.com/nilearn/nilearn/')))
+    report.add(api.Section('Contact').add(api.Paragraph('See <a href="https://github.com/nilearn/nilearn/">https://github.com/nilearn/nilearn/</a>')))
 
     return report
 
@@ -127,8 +136,6 @@ def main():
     params_file = osp.join(output_dir, 'params.json')
     if args.report_only:
         params = json.load(open(params_file))
-        img_src_filenames = [osp.join(output_dir, 'images', fname)
-                             for fname in os.listdir(osp.join(output_dir, 'images'))]
     else:
         prepare_report_directory(output_dir)
 
@@ -137,14 +144,15 @@ def main():
             canica = get_fitted_canica(func_files, **params)
             # Retrieve the independent components in brain space
             components_img = canica.masker_.inverse_transform(canica.components_)
-            img_src_filenames = generate_images(components_img, params['n_compoanents'],
-                                                osp.join(output_dir, 'images'))
+            generate_images(components_img, params['n_components'],
+                            osp.join(output_dir, 'images'),
+                            glass=True)
             json.dump(params, open(osp.join(output_dir, 'params.json'), 'w'))
-            # img src in the html needs to be relative to index.html
-            img_src_filenames = [os.path.relpath(fn, output_dir) for fn in img_src_filenames]
-
 
     if params:
+        img_src_filenames = [osp.join(output_dir, 'images', fname)
+                             for fname in os.listdir(osp.join(output_dir, 'images'))
+                             if fname.startswith('IC_')]
         report = generate_report(params, img_src_filenames)
         reportindex = osp.abspath(osp.join(output_dir, 'index.html'))
         report.save_html(reportindex)
